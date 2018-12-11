618b11934035823827999bf73ef825dc59bbbd55
JKozma81 <janos.kozma81@gmail.com>
2018-12-11 16:30:02 +0100

    Ready for review

--- controllers/projectPage.js
+++ controllers/projectPage.js
@@ -5,9 +5,6 @@ exports.getProjectPage = (req, res, next) => {
   const projectId = req.params.projectId;
   const projectsNum = projectModel.getAllData().length;
   res.locals.project = projectModel.getProjectData(projectId);
-
-  console.dir(req.params);
-  console.dir(parseInt(projectId));
   if (parseInt(projectId) > projectsNum || parseInt(projectId) < 1 || isNaN(parseInt(projectId)) || parseInt(projectId) === undefined) {
     return next();
   }
